38552622bdfbbf715b7464fbd9c85e250fcf6ce5
jakubekchr <jakubekkarminek@gmail.com>
2026-09-15 09:49:29 +0200

    cos fajnego

---
+++ src/Nauczyciel.java
@@ -0,0 +1,7 @@
+public class Nauczyciel extends Osoba {
+    private String przedmiot;
+    public Nauczyciel(String imie,String przedmiot) {
+        super(imie);
+        this.przedmiot = przedmiot;
+    }
+}
